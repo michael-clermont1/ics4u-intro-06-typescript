3b35ee6d16b15508a41d093ebabbe60bd62314f5
Michael Clermont <michael.clermont@mths.ca>
2022-09-14 01:05:01 +0000

    added hello world program

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * The program calculates the amount of logs
+ *   that can fit into a truck.
+ *
+ * By:      Michael Clermontl
+ * Version: 1.0
+ * Since:   2022-11-25
+ */
+
+console.log('Hello, World!')
+
+console.log('\nDone.')
